42501c05830f6bd0564368cb0991d83aedae6856
Luis Eduardo Brito <luiseduardo14@gmail.com>
2017-05-06 17:11:29 -0300

    Docs: Simple readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # stocks-api
 API base template for a Stocks monitoring application
+
+
+### Starting the server
+
+To run the server, install Hug then:
+
+```shell
+hug -f api.py
+```
--- api.py
+++ api.py
@@ -3,21 +3,20 @@ import json
 
 from database import User, MainDatabase
 
-@hug.get('/happy_birthday', output=hug.output_format.json)
-def happy_birthday(name, age: hug.types.number = 1):
-    return {
-        'name': name,
-        'age': age
-    }
-
-
 @hug.post('/signup', output=hug.output_format.json)
-def signup(body, charset='utf-8'):
+def user_signup(body, charset='utf-8'):
 
     user = User(email=body['email'], name=body['name'], password=body['password'])
     user.save()
 
     return json.loads(user.to_json())
+
+
+@hug.get('/user/age', output=hug.output_format.json)
+def user_age(body, charset='utf-8'):
+
+
+
 
 
 db = MainDatabase({})
